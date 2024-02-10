184b88c496edd29896c4bf3c4254b7e46dc2cf51
Luis Bazan <luis@bazan.pe>
2024-02-10 09:03:18 -0500

    Added example.env

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -1,10 +1,12 @@
 module.exports = {
   name: "guildCreate",
   run: async (client, guild) => {
-    if(!guild.me.permissions.has(process.env.BOT_PERMISSIONS)) {
+    if (!guild.me.permissions.has(process.env.BOT_PERMISSIONS)) {
       const owner = await guild.fetchOwner();
-      await owner?.send(`I don't have the permissions to join this server. Try again https://discord.com/api/oauth2/authorize?client_id=958181869646663740&permissions=${process.env.BOT_PERMISSIONS}&scope=bot%20applications.commands`);
-      return guild.leave()
+      await owner?.send(
+        `I don't have the permissions to join this server. Try again https://discord.com/api/oauth2/authorize?client_id=958181869646663740&permissions=${process.env.BOT_PERMISSIONS}&scope=bot%20applications.commands`
+      );
+      return guild.leave();
     }
     await client.resolveGuildDb(guild.id);
   },
