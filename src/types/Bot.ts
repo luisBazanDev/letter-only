1c55eba882fa6f47a9c0647966a2905ff6e50ced
Luis Bazan <luis@bazan.pe>
2024-02-24 20:04:28 -0500

    Add resolveGuild default method

--- src/types/Bot.ts
+++ src/types/Bot.ts
@@ -1,10 +1,18 @@
-import { Client, Collection } from "discord.js";
+import { Client, ClientOptions, Collection } from "discord.js";
 import Guilds from "../models/guilds";
 import { Command } from "./Command";
 
 type collection = InstanceType<typeof Collection<string, Command>>;
 
-export interface Bot extends Client {
-  commands?: collection;
-  resolveGuildDb?: (guildId: string) => Promise<InstanceType<typeof Guilds>>;
+export class Bot extends Client {
+  commands: collection;
+  resolveGuildDb: (guildId: string) => Promise<InstanceType<typeof Guilds>>;
+
+  constructor(options: ClientOptions) {
+    super(options);
+    this.commands = new Collection();
+    this.resolveGuildDb = async (guild_id: string) => {
+      return new Promise(() => {});
+    };
+  }
 }
