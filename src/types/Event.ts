5e12d3fc741701e9c15bceca0c249a0d5ff0a7f6
Luis Bazan <luis@bazan.pe>
2024-02-24 19:53:16 -0500

    Replace Event name type to ClientEvents

--- src/types/Event.ts
+++ src/types/Event.ts
@@ -3,6 +3,6 @@ import { Bot } from "./Bot";
 import { ClientEvents } from "discord.js";
 
 export interface Event {
-  name: string;
+  name: keyof ClientEvents;
   run: (client: Bot, ...args: any) => void | Promise<void>;
 }
