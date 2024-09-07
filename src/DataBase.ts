1dadd849584753a873189def62d2978772140153
Luis Bazán <luis@bazan.pe>
2024-09-07 16:50:26 -0500

    Added Database

--- src/DataBase.ts
+++ src/DataBase.ts
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 import { MONGO_URI } from "./Config";
 
 export async function connect() {
+  mongoose.set("strictQuery", false);
+
   mongoose.connect(MONGO_URI).then(() => {
     console.log("Database connected.");
   });
